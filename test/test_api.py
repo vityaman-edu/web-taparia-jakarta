fe00a6e7af54dbb582d375a04bb5036246198171
Victor Smirnov <vityaman.dev@yandex.ru>
2023-05-29 18:25:38 +0300

    [test] made tests execute lazy and moved funcs to test_lib

---
+++ test/test_api.py
@@ -0,0 +1,69 @@
+from test_lib import *
+    
+    
+if __name__ == "__main__":
+    R = 100
+
+    tester_username = "tester"
+    tester_password = "qwerty123"
+    tester_credentials = UserCredentials(tester_username, tester_password)
+
+    tester_token = register_user(tester_credentials)
+    
+    anon_token = register_user(UserCredentials("anon", "lalala"))
+
+    for action in [
+        lambda: users_get_by_name(tester_username),
+        lambda: pictures_post(tester_token, PictureDraft(
+            name = "rectangle", 
+            data = {
+                "type": "colored",
+                "color": "#0FFF00",
+                "child": {
+                    "type": "polygon",
+                    "points": [
+                        { "x": -R, "y": -R },
+                        { "x":  R, "y": -R },
+                        { "x":  R, "y":  R },
+                        { "x": -R, "y":  R },
+                    ]
+                }
+            }
+        )),
+        lambda: pictures_post(tester_token, PictureDraft(
+            name = "legacy", 
+            data = {
+                "type": "colored",
+                "color": "#FFFF00",
+                "child": {
+                    "type": "union",
+                    "children": [
+                        {
+                            "type": "ellipse",
+                            "center": {"x": 10, "y": 40},
+                            "radius": {"x": 50, "y": 50}
+                        },
+                        {
+                            "type": "ellipse",
+                            "center": {"x": 0,  "y": 0 },
+                            "radius": {"x": 50, "y": 50}
+                        }
+                    ]
+                }
+            }
+        )),
+        lambda: pictures_get_by_id(tester_token, 1),
+        lambda: pictures_get_by_id(tester_token, 2),
+        lambda: pictures_get_by_id(anon_token, 1),
+        lambda: pictures_get_all_by_owner_id(tester_token, 1),
+        lambda: pictures_taps_post(tester_token, 1, Point(0, 0)),
+        lambda: pictures_taps_post(tester_token, 1, Point(100, 100)),
+        lambda: pictures_taps_post(tester_token, 1, Point(2 * R, 2 * R)),
+        lambda: pictures_taps_get_all_by_owner_id(tester_token, 1, 1)
+    ]: 
+        try:
+            response = action()
+            print_response(response)
+        except Exception as e:
+            print('Error:', e)
+
